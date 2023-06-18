eb8a0dfa499de9757d214ecf9e904578bce3a305
Tejendra <Tejenderdanu46@gmail.com>
2023-06-18 13:17:49 +0530

    Minor chnages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
@@ -8,10 +8,12 @@ function App() {
     <>
       {/* here title is a props which will make the navbar text as dynamic*/}
       <Navbar title="TextUtils" aboutText="About" />
+      
       <div className="container my-3" >
-        {/* <TextForm heading = "Enter your text to analyze below" /> */}
-      <About/>
+        <TextForm heading = "Enter your text to analyze below" />
       </div>
+      
+      {/* <About/> */}
     </>
   );
 }
